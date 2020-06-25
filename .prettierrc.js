bf114c8d6073a7dfad778cff4503c8b3c3d612a9
Sergey Tshovrebov <sinxwal@gmail.com>
2020-06-25 23:53:40 +0300

    Ts build system added

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: "all",
+}
